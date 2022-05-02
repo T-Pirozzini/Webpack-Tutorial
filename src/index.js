3e2bbd9ed73c043069ab3837d392984b187b4c7a
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-02 11:37:59 -0700

    webpack fonts images and data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import _ from "lodash";
+// import myName from './myName';
+import './style.css';
+import Icon from './spuddy_1.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
+  // use your function!
+  // element.textContent = myName('Travis');
+  // return element;
+
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon); 
+  
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
